9fee589853aea755c801871f261f936c179164cb
Alexei <alexei.chircu@gmail.com>
2022-05-16 16:53:48 +0300

    Upload exercises from lesson 4

--- Lesson 4/Ex4/src/Main.java
+++ Lesson 4/Ex4/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int H =sc.nextInt();
+        int A =sc.nextInt();
+        int B =sc.nextInt();
+        int feettoH= H-B;
+        int feetsperday= A-B;
+        int result = feettoH%feetsperday!=0?feettoH/feetsperday +1:feettoH/feetsperday;
+        System.out.println(result);
     }
 }
